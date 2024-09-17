76c910846b39b2e4fdd31d2d45e03aa23ee26428
yayanyang <yayanyang@gmail.com>
2024-09-17 20:39:05 +0800

    update

--- crates/compositor/src/wgpu/rendering/canvas.rs
+++ crates/compositor/src/wgpu/rendering/canvas.rs
@@ -83,9 +83,9 @@ impl DriverWgpuRenderer for WgpCanvasRender {
                 ops: wgpu::Operations {
                     load: wgpu::LoadOp::Clear(wgpu::Color {
                         r: 0.1,
-                        g: 0.2,
-                        b: 0.3,
-                        a: 1.0,
+                        g: 0.1,
+                        b: 0.1,
+                        a: 0.0,
                     }),
                     store: wgpu::StoreOp::Store,
                 },
